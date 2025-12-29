e02fe0a17057f3fbc04e8b0c266cee9fb38e1601
shuyx <18271801652@163.com>
2025-12-29 17:40:21 +0800

    自动更新: 2025-12-29 17:40:20

--- pages/exam/practice/practice.vue
+++ pages/exam/practice/practice.vue
@@ -1,40 +1,29 @@
 <template>
-	<view>
-		<view style="background-color: white;padding:10px;margin: 10px;">
-			<view style="display: flex;flex-direction: row;gap: 5px;">
-					<view>
-						<uni-tag text="66" type="primary" size="mini" />
-					</view>
-					<view style="font-size: 14px;color: rgb(51, 51, 51);">
-						1.[判断题]在拉拽触电者脱离电源的过程中,救护人用双手操作,这样对救护人比较安全。
-						<br />
-						<view style="font-size: 12px;color:gray;">
-							单选题
+	<view class="page-container">
+		<scroll-view class="content" scroll-y="true">
+			<view style="" v-for="(item,index) in question_option_List" :key="item" class="content_item">
+				<view style="display: flex;flex-direction: row;gap: 5px;">
+						<view><uni-tag :text="index+1" type="primary" size="mini" /></view>
+						<view style="font-size: 14px;color: rgb(51, 51, 51);">
+							<text>{{item.question.name}}</text>
+							<view style="font-size: 12px;color:gray;">{{item.question.type_name}}</view>
 						</view>
-					</view>
+				</view>
+				<view v-if="item.question.type == 1">
+					<!-- 单选题 -->
+					<uni-data-checkbox mode="list" :map="{text:'content',value:'id'}" :localdata="item.options"  v-model="r1" ></uni-data-checkbox>
+				</view>
+				<view v-else-if="item.question.type == 2">
+					<!-- 多选题 -->
+					<uni-data-checkbox mode="list" :map="{text:'content',value:'id'}" multiple  :localdata="item.options" v-model="r2" @change="change"></uni-data-checkbox>
+				</view>
 			</view>
-			<view>
-				<uni-data-checkbox v-model="radio1" :localdata="sex" mode="list"></uni-data-checkbox>
-			</view>
-		</view>
-		<view style="background-color: white;padding:10px;margin: 10px;">
-			<view style="display: flex;flex-direction: row;gap: 5px;">
-					<view>
-						<uni-tag text="66" type="primary" size="mini" />
-					</view>
-					<view style="font-size: 14px;color: rgb(51, 51, 51);">
-						1.[判断题]在拉拽触电者脱离电源的过程中,救护人用双手操作,这样对救护人比较安全。
-						<br />
-						<view style="font-size: 12px;color:gray;">
-							多选题
-						</view>
-					</view>
-			</view>
-			<view>
-				<uni-data-checkbox multiple v-model="value" :localdata="sex" @change="change" mode="list"></uni-data-checkbox>
-			</view>
+		</scroll-view>
+		<view class="footer">
+			<button class="footer-button" @click="toSubmit">固定按钮</button>
 		</view>
 	</view>
+	
 </template>
 
 <script setup>
@@ -53,33 +42,68 @@ onLoad((option) => {
 onMounted(()=>{
 	getQuestionList()
 })
-	
-let sex = ref([{
-			text: '男',
-			value: 0
-		}, {
-			text: '女',
-			value: 1
-		}, {
-			text: '未知',
-			value: 2
-		}]) 
 		
-let value = ref([0,2])
-let range = ref([{"value": 0,"text": "篮球"	},{"value": 1,"text": "足球"},{"value": 2,"text": "游泳"}])
+
+// 问题选项列表
+let question_option_List = ref([
+	{
+		question:{},
+		options:[]
+	}
+])
+
+let r1 = {}
+let r2 = []
+
 
 function getQuestionList(){
 	let params = {exam_id:examId.value}
 	ExamAPIService.getQuestionList(params).then((res) => {
 		console.log(res)
 		if (res.code == 200) {
-			
+			question_option_List.value = res.data
 		}
 	})
 }
 
+function toSubmit(){
+	console.log(r1.value)
+	console.log(r2.value)
+}
+
 </script>
-<style>
-	       
+<style scoped>
+page-container {
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+}
+
+content {
+	flex: 1;
+	overflow-y: auto;
+}
+
+content_item {
+	background-color: white;
+	padding:10px;
+	margin: 10px;
+}
+	
+footer {
+	height: 60px;
+	background-color: #f8f8f8;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+footer-button {
+	width: 100px;
+	height: 40px;
+	background-color: #007AFF;
+	color: white;
+	border-radius: 5px;
+}   
 </style>
   
